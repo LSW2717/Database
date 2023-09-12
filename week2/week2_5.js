c219bee8c1ec47430a8fcbf28d9601f885830e4b
Leesiwon12171260 <siwon2717@gmail.com>
2023-09-12 18:55:11 +0900

    3week

--- week2/week2_5.js
+++ week2/week2_5.js
@@ -1,29 +1,32 @@
-$(document).ready(function () {
-    const display = $("#js-result");
-    const range = $("#js-range");
-    const rangeValue = $("#max");
-    const guessInput = $("#num");
+const display = document.getElementById("js-result");
+const range = document.getElementById("js-range");
+const guessInput = document.getElementById("num");
+const rangeValue = document.getElementById("max");
 
-    function generateRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function updateRangeValue() {
+    rangeValue.textContent = range.value;
+}
+const handlePrint = (e) => {
+    e.preventDefault();
+    const userGuess = guessInput.value;
+    const computerGuess = generateRandomNumber(0, range.value);
+    const displaySpan = display.querySelector("span");
+
+    let resultMessage = " ";
+    resultMessage = `You chose: ${userGuess}, the machine chose: ${computerGuess}.`;
+
+    if (userGuess == computerGuess) {
+        resultMessage += "<br><strong>You win!</strong>";
+    } else {
+        resultMessage += "<br><strong>You lose!</strong>";
     }
+    displaySpan.innerHTML = resultMessage;
+    updateRangeValue();
+};
+const printForm = document.getElementById("js-guess")
+printForm.addEventListener("submit", handlePrint);
 
-    range.on("input", function () {
-        rangeValue.text(range.val());
-    });
-
-    $("#js-guess").submit(function (e) {
-        e.preventDefault();
-        const userGuess = parseInt(guessInput.val());
-        const computerGuess = generateRandomNumber(0, parseInt(range.val()));
-
-        let resultMessage = "";
-        resultMessage = `You chose: ${userGuess}, the machine chose: ${computerGuess}.`;
-        if (userGuess === computerGuess) {
-            resultMessage += "<br><strong>You win!</strong>";
-        } else {
-            resultMessage += "<br><strong>You lose!</strong>";
-        }
-        display.find("span").html(resultMessage);
-    });
-});
---
+++ week2_2/sql.js
@@ -0,0 +1,22 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'tldnjs0807',
+    database: 'week3_company',
+})
+
+const promisePool = pool.promise();
+
+const sql = {
+    getEmployee: async () => {
+        const results = await promisePool.query(`
+        select * from employee
+        `)
+        return results;
+    },
+};
+
+export default sql;
